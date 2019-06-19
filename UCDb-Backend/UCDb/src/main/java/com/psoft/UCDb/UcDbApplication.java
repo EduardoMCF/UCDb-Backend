3cc1842b8098efceb928e79ec3dea1d0b7f2abc4
Eduardo Cavalcanti <eduardo.freitas@ccc.ufcg.edu.br>
2019-06-19 12:43:28 -0300

    US1 testada e operante

---
+++ UCDb-Backend/UCDb/src/main/java/com/psoft/UCDb/UcDbApplication.java
@@ -0,0 +1,25 @@
+package com.psoft.UCDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.psoft.UCDb.rest.filters.TokenFilter;
+
+@SpringBootApplication
+public class UcDbApplication {
+	
+	@Bean
+	public FilterRegistrationBean filterJwt() {
+		FilterRegistrationBean filterRb = new FilterRegistrationBean();
+		filterRb.setFilter(new TokenFilter());
+		filterRb.addUrlPatterns("/teste");
+		return filterRb;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(UcDbApplication.class, args);
+	}
+
+}
