b8abc38f57787e194af5461eb92deb5f52d07f17
Gasparsa <34282336+Gasparsa@users.noreply.github.com>
2019-06-20 19:41:43 -0300

    subjectService

--- UCDb/src/main/java/com/psoft/UCDb/service/SubjectService.java
+++ UCDb/src/main/java/com/psoft/UCDb/service/SubjectService.java
@@ -1,5 +1,25 @@
 package com.psoft.UCDb.service;
 
+import com.psoft.UCDb.rest.dao.SubjectDAO;
+import com.psoft.UCDb.rest.model.Subject;
+
 public class SubjectService {
+	private final SubjectDAO subjectDAO;
+
+	SubjectService(SubjectDAO subjectDAO) {
+		this.subjectDAO = subjectDAO;
+	}
+
+	public Subject create(Subject subject) {
+		return subjectDAO.save(subject);
+	}
+
+	public void deleteById(int id) {
+		subjectDAO.deleteById(id);
+	}
+
+	public Subject findById(int id) {
+		return subjectDAO.findById(id);
+	}
 
 }
