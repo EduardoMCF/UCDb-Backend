eff4214eb9e1d063a6473f91d4d88556438092ea
Gasparsa <34282336+Gasparsa@users.noreply.github.com>
2019-06-20 19:42:59 -0300

    CommentService

--- UCDb/src/main/java/com/psoft/UCDb/service/CommentService.java
+++ UCDb/src/main/java/com/psoft/UCDb/service/CommentService.java
@@ -1,5 +1,27 @@
 package com.psoft.UCDb.service;
 
+import java.util.Date;
+
+import com.psoft.UCDb.rest.dao.CommentDAO;
+import com.psoft.UCDb.rest.model.Comment;
+
 public class CommentService {
+	private final CommentDAO commentDAO;
+
+	CommentService(CommentDAO commentDAO) {
+		this.commentDAO = commentDAO;
+	}
+
+	public Comment create(Comment comment) {
+		return commentDAO.save(comment);
+	}
+
+	public Comment findById(String msg, User user, Date date) {
+		return commentDAO.findById(msg, user, date);
+	}
+
+	public void deleteById(String msg, User user, Date date) {
+		commentDAO.deleteById(msg, user, date);
+	}
 
 }
