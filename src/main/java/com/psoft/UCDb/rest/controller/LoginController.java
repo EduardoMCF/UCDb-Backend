bb8c39af7a3ba29c67cce7296ac361dae733a8d3
Eduardo Cavalcanti <eduardo.freitas@ccc.ufcg.edu.br>
2019-07-04 00:30:10 -0300

    Mudança na duração do token

--- src/main/java/com/psoft/UCDb/rest/controller/LoginController.java
+++ src/main/java/com/psoft/UCDb/rest/controller/LoginController.java
@@ -47,7 +47,7 @@ public class LoginController {
         String token = Jwts.builder().
                 setSubject(authUser.getEmail()).
                 signWith(SignatureAlgorithm.HS512, TOKEN_KEY).
-                setExpiration(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
                 .compact();
         
         return new LoginResponse(token);
